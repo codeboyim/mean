310a9a2192494a0dc12730fc4a4648544744e1c5
Codeboy Me <me@codeboy.im>
2014-03-20 16:29:03 +1100

    add 'api' folder, change 'articles' to api fashion

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Articles routes use articles controller
-var articles = require('../controllers/articles');
+var articles = require('../api/articles');
 var authorization = require('./middlewares/authorization');
 
 // Article authorization helpers
@@ -14,11 +14,12 @@ var hasAuthorization = function(req, res, next) {
 
 module.exports = function(app) {
 
-    app.get('/articles', articles.all);
-    app.post('/articles', authorization.requiresLogin, articles.create);
-    app.get('/articles/:articleId', articles.show);
-    app.put('/articles/:articleId', authorization.requiresLogin, hasAuthorization, articles.update);
-    app.del('/articles/:articleId', authorization.requiresLogin, hasAuthorization, articles.destroy);
+    // articles api
+    app.get('/api/articles', articles.all);
+    app.post('/api/articles', authorization.requiresLogin, articles.create);
+    app.get('/api/articles/:articleId', articles.show);
+    app.put('/api/articles/:articleId', authorization.requiresLogin, hasAuthorization, articles.update);
+    app.del('/api/articles/:articleId', authorization.requiresLogin, hasAuthorization, articles.destroy);
 
     // Finish with setting up the articleId param
     app.param('articleId', articles.article);
