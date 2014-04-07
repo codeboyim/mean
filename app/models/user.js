f9275339a6f04cf1b56d90963f5152e221ca9cdf
codeboy <me@codeboy.im>
2014-04-07 19:35:49 +1000

    added mandatory to email in User model

--- app/models/user.js
+++ app/models/user.js
@@ -15,7 +15,10 @@ var UserSchema = new Schema({
         type: String,
         required: true
     },
-    email: String,
+    email: {
+        type: String,
+        required: true
+    },
     username: {
         type: String,
         unique: true
