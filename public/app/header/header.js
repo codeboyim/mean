c00f7cbc0c67720425e1c33547a1a6648c94513e
codeboy <me@codeboy.im>
2014-04-15 22:46:24 +1000

    removed header from partial views

--- public/app/header/header.js
+++ public/app/header/header.js
@@ -3,18 +3,6 @@ angular.module('mean').controller('HeaderController', [
   '$scope', 'Auth', function($scope, Auth) {
     $scope.user = Auth.currentUser;
     $scope.authenticated = Auth.isLoggedIn;
-    $scope.menu = [
-      {
-        'title': 'Articles',
-        'link': 'articles'
-      }, {
-        'title': 'Create New Article',
-        'link': 'articles/create'
-      }, {
-        'title': 'Admin',
-        'link': 'admin'
-      }
-    ];
     return null;
   }
 ]);
--- public/coffee/header/header.coffee
+++ public/coffee/header/header.coffee
@@ -7,11 +7,6 @@ angular.module('mean').controller('HeaderController',
 		($scope, Auth)->
 			$scope.user = Auth.currentUser
 			$scope.authenticated = Auth.isLoggedIn
-			$scope.menu = [
-				{'title': 'Articles','link': 'articles'}
-				{'title': 'Create New Article','link': 'articles/create'}
-				{'title': 'Admin','link': 'admin'}
-			]
 
 			null
 	]
