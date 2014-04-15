54bcf3ab2764cc077b0709311dc693e521169e10
codeboy <me@codeboy.im>
2014-04-15 18:14:34 +1000

    created HeaderController

---
+++ public/app/header/header.js
@@ -0,0 +1,20 @@
+'use strict';
+angular.module('mean').controller('HeaderController', [
+  '$scope', 'Auth', function($scope, Auth) {
+    $scope.user = Auth.currentUser;
+    $scope.authenticated = Auth.isLoggedIn();
+    $scope.menu = [
+      {
+        'title': 'Articles',
+        'link': 'articles'
+      }, {
+        'title': 'Create New Article',
+        'link': 'articles/create'
+      }, {
+        'title': 'Admin',
+        'link': 'admin'
+      }
+    ];
+    return null;
+  }
+]);
---
+++ public/coffee/header/header.coffee
@@ -0,0 +1,19 @@
+'use strict'
+
+angular.module('mean').controller('HeaderController',
+	[
+		'$scope'
+		'Auth'
+		($scope, Auth)->
+
+			$scope.user = Auth.currentUser
+			$scope.authenticated = Auth.isLoggedIn()
+			$scope.menu = [
+				{'title': 'Articles','link': 'articles'}
+				{'title': 'Create New Article','link': 'articles/create'}
+				{'title': 'Admin','link': 'admin'}
+			]	
+
+			null
+	]
+)
